3fb773b15d5e0695aae4487a776d45182ba91853
Andre Serudo <andreserudo@gmail.com>
2020-08-05 18:23:03 -0400

    componentes da pagina TeacherList

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -4,6 +4,7 @@ import PageHeader from '../../components/PageHeader';
 
 import './styles.css';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 function TeacherList(){
   return (
@@ -11,20 +12,11 @@ function TeacherList(){
       <PageHeader title="Estes são os proffy disponiveis.">
         <form id="search-teachers">
 
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"/>
-          </div>
+          <Input name="subject" label="Matéria" />
 
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana:</label>
-            <input type="text" id="week_day"/>
-          </div>
+          <Input name="week_day" label="Dia da semana" />
 
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time"/>
-          </div>
+          <Input name="time" label="Hora" />
 
         </form>
       </PageHeader>
