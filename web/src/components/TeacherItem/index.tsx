8a1e93114eb4c5c6000b533f0d55f3b450539714
Andre Serudo <andreserudo@gmail.com>
2020-08-05 22:44:13 -0400

    whatsapp feature e conexoes

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 export interface Teacher {
   id: number;
@@ -19,6 +20,12 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher}) => {
+
+  function createNewConnection(){
+    api.post('connections',{
+      user_id: teacher.id
+    });
+  }
 
   return( 
     <article className="teacher-item">
@@ -39,10 +46,10 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher}) => {
           Preço/hora
           <strong>R$80,00</strong>
         </p>
-        <button type="button">
+        <a onClick={createNewConnection} href={`httpw://wa.me/${teacher.whatsapp}`} target="_blank">
           <img src={whatsappIcon} alt="WhatsApp Ícone"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
